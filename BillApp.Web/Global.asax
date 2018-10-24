c11e4e4900d9208b768f7994187a3ea220764a52
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-10-24 15:56:43 -0500

    Initial commit, Empty projects

---
+++ BillApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BillApp.Web.MvcApplication" Language="C#" %>
---
+++ BillApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BillApp.Web.Startup))]
+namespace BillApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
