337314346648aed650caadfa51299e6b7b5aa8bc
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-10-26 14:16:21 -0500

    Add template bootstrap and Domain

---
+++ BillApp.Domain/InvoiceHeader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillApp.Domain
+{
+    public class RasonSocial
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string NIT { get; set; }
+
+        public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Email { get; set; }
+    }
+}
