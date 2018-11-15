bb2c31ce022f2045ceb6fd6a3656670a35f175f4
Mario Andres Botero Florez <mariobot113@hotmail.com>
2018-11-15 17:29:20 -0500

    correct the validation in ajax

--- BillApp.Domain/InvoiceItem.cs
+++ BillApp.Domain/InvoiceItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,17 @@ namespace BillApp.Domain
 
         public string item { get; set; }
 
+        [Required(ErrorMessage = "* Requerido")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "Solo Numeros")]
+        [Range(1,9999,ErrorMessage ="Valor fuera del rango 1 - 9999")]        
         public float Quanty { get; set; }
 
+        [Required(ErrorMessage = "* Requerido")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "Solo Numeros")]
         public float ValueUnit { get; set; }
 
+        [Required(ErrorMessage = "* Requerido")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "Solo numeros")]
         public float ValueTotal { get; set; }
 
         public int InvoiceId { get; set; }
